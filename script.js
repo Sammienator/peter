05636e2d5492bd1d2d63b1b1db39133aaa478cc1
Sammienator <sammugo18@gmail.com>
2022-02-19 12:53:08 +0300

    changes

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
- 
+ $(window).scroll(function(){
+      if($(window).scrollTop() > 0){
+          $('.navbar').css('background','#212529');
+      }
+      else{
+        $('.navbar').css('background','transparent');
+      }
+ });
